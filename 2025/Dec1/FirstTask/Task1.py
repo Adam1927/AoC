48e4fc8ff9f226e0f16ae2356d96c8d4d257b909
faundez <faundez@student.chalmers.se>
2025-12-01 17:51:52 +0100

    Add solution for first task of December 1st

---
+++ 2025/Dec1/FirstTask/Task1.py
@@ -0,0 +1,28 @@
+code_counter = 0
+current_dial_position = 50 
+encrypted_file = open('2025/Dec1/FirstTask/task1Input.txt', 'r')
+file_lines = encrypted_file.readlines()
+encrypted_file.close()
+
+for line in file_lines:
+    letter = ""
+    number = ""
+    for character in line.replace("\n", ""):
+        if character.isdigit():
+            number += character
+        else:
+            letter = character
+    old_dial_position = current_dial_position
+    if letter == "R":
+        current_dial_position = (int(number) + old_dial_position) % 100
+    else:
+        number = (int(number) % 100)
+        current_dial_position = old_dial_position - number
+        if number > old_dial_position:
+            current_dial_position += 100
+    if current_dial_position == 0:
+        code_counter += 1
+    print(current_dial_position)
+
+
+print("The code is: " + str(code_counter))
